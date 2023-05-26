37681468677252c280f951ce5cae283ab7cf0eb4
SuzanskayaNA <suzanskayana@gmail.com>
2023-05-26 13:03:49 +0300

    created a database schema, created insert statements

---
+++ readme.md
@@ -0,0 +1,12 @@
+TASK:
+Create a database schema using Mysql Workbench for the new hierarchy with at least 12 tables and all relations (done)
+Create for your hierarchy:
++ 10 statements for insertion. (done)
+- 10 statements for updating.
+- 10 statements for deletions.
++ 5 alter table.                (done)
+- 1 big statement to join all tables in the database.
+- 5 statements with left, right, inner, outer joins.
+- 7 statements with aggregate functions and group by and without having.
+- 7 statements with aggregate functions and group by and with having.
+
---
+++ statements.sql
@@ -0,0 +1,19 @@
+/*insert into client_type values (1, "Individual");
+insert into client_type values (2, "Business");
+ insert into client values  (1, "Jhon","NB2323536","+235354646","NY",1);
+ ALTER TABLE deposit_type MODIFY COLUMN replenishment int;
+ ALTER TABLE deposit_type MODIFY COLUMN replenishment BOOLEAN;
+ ALTER TABLE deposit_type DROP COLUMN description;
+insert into deposit_type values  (1, "START",5,36,1,0);
+insert into deposit_type values  (2, "SUMMER",57,3,1,1);
+insert into deposit values  (1, 100,"2023-05-25","2023-08-25",1,1);
+insert into client values  (2, "Doris","SD4654757","+747536457","Warsaw",1);
+insert into client values  (3, "Alex","QW87677777","+7657354657","Oslo",2);
+ALTER TABLE credittype RENAME COLUMN id_creditType to id_credit_type;
+insert into credittype values  (1, "HOT","Account in USD, EUR",13,18);
+insert into credittype values  (2, "BUY CAR","Account in USD, EUR",15,24);*/
+insert into credit values  (1, 10000,"2023-06-01","2024-06-30",1,2);
+ALTER TABLE operation_type DROP COLUMN operation_typecol;
+
+
+
